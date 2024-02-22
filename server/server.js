37dcdecccd1b70ec5173a2e902a34a4c2834ff8e
Damon248 <dhruvinthakkar248@gmail.com>
2024-02-22 22:18:03 +0530

    m

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 connectToDB();
 
-const port = process.env.PORT || 5000;
+const port = process.env.port || 5000;
 app.get("/", (req, res) => {
   res.send("API Running!");
 });
