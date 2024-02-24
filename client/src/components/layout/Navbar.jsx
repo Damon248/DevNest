b6c454306d25d6cb97c70b2caf5fe650b473cc08
Damon248 <dhruvinthakkar248@gmail.com>
2024-02-24 15:21:15 +0530

    a minor bug fixed

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
   const guestLinks = (
     <ul>
       <li>
-        <a href="/profiles">Developers</a>
+        <Link to="/profiles">Developers</Link>
       </li>
       <li>
         <Link to="/register">Register</Link>
